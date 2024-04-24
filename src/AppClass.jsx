29e96eef20f07e130299a13ee7ebff303f3c4193
Saranya2024 <159130151+Saranya2024@users.noreply.github.com>
2024-04-24 18:05:37 +0530

    Update AppClass.jsx

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,40 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import "./App.css"
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant
       },
       {
-        id:4,
-        img:elephant
+        id: 4,
+        img: elephant
       }
     ]
     return data;
   }
 
-  // code here
+  render() {
+    const images = this.imageData();
+    
+    return (
+      <div className="App">
+        <h1>Kalvium Gallary</h1>
+        {images.map(image => (
+          <img key={image.id} src={image.img} alt={`Elephant ${image.id}`} />
+        ))}
+      </div>
+    );
+  }
 }
