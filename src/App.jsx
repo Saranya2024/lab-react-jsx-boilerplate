11c6647caa40754dde1468d507c1f3388cb01fb8
Saranya2024 <159130151+Saranya2024@users.noreply.github.com>
2024-04-24 18:05:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
+import React from 'react';
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant
     },
     {
-      id:4,
-      img:elephant
+      id: 4,
+      img: elephant
     }
   ]
   return data;
 }
 
 function App() {
-  // code here
+  const images = imageData();
+
+  return (
+    <div className="App">
+      <h1>Kalvium Gallary</h1>
+      {images.map(image => (
+        <img key={image.id} src={image.img} alt={`Elephant ${image.id}`} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
